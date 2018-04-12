887ca6ec983adb8c57ef30b62702604c811fd32a
alvarobasi <36459941+alvarobasi@users.noreply.github.com>
2018-04-12 03:37:39 +0200

    Para el branch

--- public/js/funcionesJquery.js
+++ public/js/funcionesJquery.js
@@ -1,163 +1,286 @@
 $(document).ready(function(){
-
-	var modificarTabla = function(producto, cantidad, precio){
-		var precioBicho = 0;
-		var cantidadBicho = 0;
-		var totalBicho = 0;
-		if (producto === "toro") {
-			precioBicho = $("#precioÑu");
-			cantidadBicho = $("#cantidadÑu");
-			totalBicho = $("#totalÑu")
-		} else if (producto === "mono") {
-			precioBicho = $("#precioLemur");
-			cantidadBicho = $("#cantidadLemur");
-			totalBicho = $("#totalLemur")
-		} else {
-			precioBicho = $("#precioKiwi");
-			cantidadBicho = $("#cantidadKiwi");
-			totalBicho = $("#totalKiwi")
-		}
-		precioBicho.empty();
-		precioBicho.append(precio);
-		cantidadBicho.empty();
-		cantidadBicho.append(cantidad);
-
-		total = cantidad * precio;
-		totalBicho.empty();
-		totalBicho.append(total);
-
-		var precioCarro = 0;
-		//hasta aqui bien
-		precioCarro = parseInt($("#totalÑu").text())+parseInt($("#totalKiwi").text())+parseInt($("#totalLemur").text());
-		
-		$("#precioDelCarro").empty();
-		$("#precioDelCarro").append("<p>El precio del carrito para los objetos acumulados es: <h3>"+precioCarro+" euros</h3>");
-	};
-	var funPrecio = function (imagen) {
-		var objetivo = $("#objetivo1");
-		objetivo.empty();
-		
-		var precio = 0;
-		var valor = 0;
-		if (imagen === "toro") {
-			precio = 15;
-			valor = $("#cantidadÑu").text();
-		} else if (imagen === "mono") {
-			precio = 10;
-			valor = $("#cantidadLemur").text();
-		} else {
-			precio = 5;
-			valor = $("#cantidadKiwi").text();
-		}
-		var frase = "El precio es de "+precio+" euros";
-		var contenido = '<p>'+frase+'</p>';
-		objetivo.append(contenido);
-		objetivo.append('<p>Unidades:<input class="cantidad" data-producto="'+imagen+'" data-precio="'+precio+'" type="number" name="unidades" value="'+valor+'" min="0">');
-		
-		var $input = $(".cantidad");
-
-		$input.on('keyup mouseup',function(event){
-			var precio = $(this).attr("data-precio");
-			var producto = $(this).attr("data-producto");
-			var cantidad = $(this).val();
-			$(this).attr("value",cantidad);
-			modificarTabla(producto,cantidad,precio);
-		});
-	};
-
-	var funDescripcion = function (imagen) {
-		var objetivo = $("#objetivo1");
-		objetivo.empty();
-		var contenido;
-		if (imagen === "toro") {
-			contenido = "Realmente no se trata de un toro famelico, sino de un ñu bien hermoso con unos cuernecitos que para que vamos a hablar, un gusto tener un ñu así por casa, cariñoso con los niños, te rellena la quiniela, el mejor amigo del hombre sin duda.";
-		} else if (imagen === "mono") {
-			contenido = "Decir que se trata de un tipo de mono es una aberración. Los lémures son unos primates estrepsirrinos endémicos de la isla de Madagascar. Reciben su nombre por los lémures, fantasmas o espíritus de la mitología romana, debido a las estrepitosas vocalizaciones que emiten, sus ojos brillantes y los hábitos nocturnos de algunas de sus especies. Aunque a menudo se les confunda con primates ancestrales, los primates antropoides (monos, hominoides y seres humanos) no evolucionaron de los lémures, aunque comparten rasgos morfológicos y de comportamiento con primates basales.";
-		} else {
-			contenido = "Esto es un kiwi, imagino que también se puede tomar de postre pero seguramente sería un poco turbio";
-		}
-		var frase = '<p>'+contenido+'</p>';
-		objetivo.append(frase);
+	window.emojiPicker = new EmojiPicker({
+		emojiable_selector: '[data-emojiable=true]',
+		assetsPath: '../lib/img/',
+		popupButtonClasses: 'fa fa-smile-o'
+	});
+	window.emojiPicker.discover();
+	
+	function bajarScroll(){
+		var element = $('#mensajesRecibidos');
+		element.animate({scrollTop: element.prop('scrollHeight')});
+		$('html,body').animate({scrollTop: document.body.scrollHeight});
 	}
 
-	var funImagen = function (imagen) {
-		var objetivo = $("#objetivo1");
-		objetivo.empty();
+	var socket = io.connect({'sync disconnect on unload':true});
 
-		var imagenSec1 = $("#imagenSec1");
-		var linkImg1;
-		var imagenSec2 = $("#imagenSec2");
-		var linkImg2;
-		var imagenSec3 = $("#imagenSec3");
-		var linkImg3;
-		 //Hasta aqui bien
-		if (imagen === "toro") {
-			linkImg1 = "Imagenes/nu.jpeg";
-			linkImg2 = "Imagenes/nu2.jpeg";
-			linkImg3 = "Imagenes/nu3.jpeg";
+	socket.on('volverAPedir', function(datos){
+		var usuario = prompt(datos);
+		console.log("Emitiendo nombre usuario");
+		socket.emit('nick', {nombreUser: usuario});
+		console.log(usuario);
+	});
+	var miNombre;
+	var miColor;
+	var padre = $('#usuariosConectados');
+	var tamVentana = $(window).height();
+	$('#mensajesRecibidos').css("max-height",tamVentana*0.9*0.7);
 
-		} else if (imagen === "mono") {
-			linkImg1 = "Imagenes/lemur.jpeg";
-			linkImg2 = "Imagenes/lemur2.jpeg";
-			linkImg3 = "Imagenes/lemur3.jpeg";
-		} else {
-			linkImg1 = "Imagenes/kiwi.jpg";
-			linkImg2 = "Imagenes/kiwi2.jpeg";
-			linkImg3 = "Imagenes/kiwi3.jpeg";
-		}
-		objetivo.append('<div id="galeriaImagenes row">\
-						<div class="col-12">\
-							<img src="'+linkImg1+'" id="imagenPrincipal" width="200" height="200">\
-						</div>\
-						<div class="col-12 row">\
-							<div class="d-inline bg-success">\
-								<img src="'+linkImg1+'" id="imagenSec1" width="42" height="42" class="d-inline-block imagenSecu">\
-							</div>\
-							<div class="d-inline bg-success">\
-								<img src="'+linkImg2+'" id="imagenSec2" width="42" height="42" class="d-inline-block imagenSecu">\
-							</div>\
-							<div class="d-inline bg-success">\
-								<img src="'+linkImg3+'" id="imagenSec3" width="42" height="42" class="d-inline-block imagenSecu">\
-							</div>\
-						</div>\
-					</div>');
+
+	socket.on('ponteAlDia', function(conectados, mensajes, nombre, color){
+		console.log('Esta conectado '+conectados[0].id);
+		console.log(mensajes[0]);
+		miNombre = nombre;
+		miColor = color;
+		var numero = 1;
+		conectados.forEach(function(conectado){
+			var nombre = conectado.id;
+			var nombreSin = nombre.replace(/ /g, '_');
+			var frase = `
+				<tr id="${nombre}" data-numero="${numero}">
+					<th scope="row">${numero}</th>
+					<th>${nombre}</th>
+					<th id="estado${nombreSin}">Conectado</th>
+				</tr>
+			`;
+			padre.append(frase);
+			numero++;
+		});
+		mensajes = mensajes.reverse();
+		mensajes.forEach(function(cadaMensaje){
+			var anadir2= `
+				<div class='offset-3 col-6 text-left text-dark' style="margin-top:5px;margin-bottom:5px;border-left: 6px solid black;
+    background-color: lightgrey;box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 3px 10px 0 rgba(0, 0, 0, 0.19);">
+					<p>${cadaMensaje.usuario}</p>
+					<div style="overflow-wrap:break-word">${cadaMensaje.mensajeEnviar}</div>
+					<p>${cadaMensaje.fecha}</p>
+				</div>
+			`;
+			$('#mensajesRecibidos').append(anadir2);
+			bajarScroll();
+		});
+		var anadir= `
+			<div class="col-12 text-center">
+					<hr>
+					<p>Mensajes Antiguos</p>
+					<hr>
+			</div>
+			`;
+		$('#mensajesRecibidos').append(anadir);
+		bajarScroll();
 		
-		var cambiarImgPrin = $(".imagenSecu");
-		cambiarImgPrin.on('click',function(){
-			var $src = $(this).attr('src');
-			var imagenPrin = $("#imagenPrincipal");
-			imagenPrin.attr("src",$src);
+	});
+
+	socket.on('clienteDesconectado', function(clientes, nombreDesc){
+		console.log('Se ha desconectado '+nombreDesc);
+		padre.empty();
+		var numero = 1;
+		clientes.forEach(function(conectado){
+			var nombre = conectado.id;
+			var nombreSin = nombre.replace(/ /g, '_');
+			var frase = `
+				<tr id="${nombre}" data-numero="${numero}">
+					<th scope="row">${numero}</th>
+					<th>${nombre}</th>
+					<th id="estado${nombreSin}">Conectado</th>
+				</tr>
+			`;
+			padre.append(frase);
+			numero++;
 		});
-	};
+		var anadir= `
+			<div class="col-12 text-center">
+					<hr>
+					<p>Se ha desconectado el usuario ${nombreDesc}</p>
+					<hr>
+			</div>
+			`;
+		$('#mensajesRecibidos').append(anadir);
+		bajarScroll();
+	});
 
-	var productos = $(".producto");
-	productos.on('click', function(){
-		$("#botonera").show();
-		var objetivo = $("#objetivo1");
-		objetivo.empty();
-		var $atributo = $(this).attr('data-imagen');
+	socket.on('nuevoClienteConectado', function(clientes, nombreCon){
+		console.log('Se ha conectado '+nombreCon);
+		padre.empty();
+		var numero = 1;
+		clientes.forEach(function(conectado){
+			var nombre = conectado.id;
+			var nombreSin = nombre.replace(/ /g, '_');
+			var frase = `
+				<tr id="${nombre}" data-numero="${numero}">
+					<th scope="row">${numero}</th>
+					<th>${nombre}</th>
+					<th id="estado${nombreSin}">Conectado</th>
+				</tr>
+			`;
+			padre.append(frase);
+			numero++;
+		});
+		var anadir= `
+				<div class="col-12 text-center">
+					<hr>
+					<p>Se ha conectado el usuario ${nombreCon}</p>
+					<hr>
+				</div>
+			`;
+		$('#mensajesRecibidos').append(anadir);
+		bajarScroll();
+	});
 
-		objetivo.append("<p>Producto: "+$atributo+"</p>");
+	$('#formMensaje').on('submit', function(event) {
+		event.preventDefault();
+		var time = new Date();
+		var hora = time.getHours();
+		var min = time.getMinutes();
+		var fecha = hora+":"+min;
+		var nuevoMensaje = $('#formMensaje textarea[name=mensaje]').val();
+		console.log(miColor);
+		var anadir= `
+			<div class='offset-4 col-6  text-right text-dark' style="margin-top:5px;margin-bottom:5px;border-right: 6px solid;border-right-color: ${miColor};
+    background-color: #c6e4ff;box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 3px 10px 0 rgba(0, 0, 0, 0.19);">
+				<p>Yo</p>
+				<div style="overflow-wrap:break-word">${nuevoMensaje}</div>
+				<p>${fecha}</p>
+			</div>
+		`;
+		$('#mensajesRecibidos').append(anadir);
+		bajarScroll();
+		var fechaAbsoluta = new Date().getTime();
+		console.log(fechaAbsoluta);
+		socket.emit('enviarMensajeChat', {mensajeEnviar: nuevoMensaje, fecha: fecha, usuario: miNombre, color: miColor, fechaAbsoluta: fechaAbsoluta});
+		$('#formMensaje').each(function(){
+			this.reset();
+		});
+		$('.mensajeUser').empty();
+	});
 
-		var $botonPrecio = $("#precio");
-		$botonPrecio.off('click');
-		$botonPrecio.on('click', function(event){
+	socket.on('nuevoMensajeChat', function(datos){
+		var anadir2= `
+			<div class='offset-2 col-6 text-left text-dark' style="margin-top:5px;margin-bottom:5px;border-left: 6px solid;border-left-color: ${datos.color};
+    background-color: lightgrey;box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 3px 10px 0 rgba(0, 0, 0, 0.19);">
+				<p>${datos.usuario}</p>
+				<div style="overflow-wrap:break-word">${datos.mensajeEnviar}</div>
+				<p>${datos.fecha}</p>
+			</div>
+		`;
+		$('#mensajesRecibidos').append(anadir2);
+		bajarScroll();
+	});
+
+	var inicioTemp = false;
+	var tiempo = 0;
+	$('.mensajeUser').keypress(function(event) {
+		var nombreSin = "#estado"+miNombre.replace(/ /g, '_');
+		socket.emit('usuarioEscribiendo', nombreSin);
+		$(nombreSin).empty();
+		$(nombreSin).append("Escribiendo");
+		if (inicioTemp){
+			console.log("Hola, reseteando");
+			clearTimeout(tiempo);
+		}
+		inicioTemp = true;
+		tiempo = setTimeout(function(){
+			console.log("Hola, haciendo");
+			$(nombreSin).empty();
+			$(nombreSin).append("Conectado");
+			inicioTemp = false;
+		},2500);
+	});
+
+	var inicioTemp2 = false;
+	var tiempo2 = 0;
+	socket.on('escribiendo', function(nombreSin){
+		$(nombreSin).empty();
+		$(nombreSin).append("Escribiendo");
+		if (inicioTemp2){
+			console.log("Hola, reseteando");
+			clearTimeout(tiempo2);
+		}
+		inicioTemp2 = true;
+		var tiempo2 = setTimeout(function(){
+			console.log("Hola, haciendo");
+			$(nombreSin).empty();
+			$(nombreSin).append("Conectado");
+			inicioTemp2 = false;
+		},2500);
+	});
+
+	$('#formRespuestaJuego').hide();
+	var jugando = false;
+	var juegoMio = false;
+	$('#formJuegoAhorcado').on('submit', function(event) {
+		if (!jugando){
 			event.preventDefault();
-			funPrecio($atributo);
-		});
+			var nuevoMensaje = $('#formJuegoAhorcado input[name=palabraAhorcado]').val();
+			var anadir= `
+				<div class='col-12>
+					Ahora estan jugando a un juego
+				</div>
+			`;
+			jugando = true;
+			juegoMio = true;
+			$('#formJuegoAhorcado').hide();
+			$('#formJuegoAhorcado').each(function(){
+				this.reset();
+			});
+			bajarScroll();
+			socket.emit('empezarJuego', {palabra: nuevoMensaje});
+		}
+	});
 
-		var $botonDescripcion = $("#descripcion");
-		$botonDescripcion.off('click');
-		$botonDescripcion.on('click', function(event){
-			event.preventDefault();
-			funDescripcion($atributo);
-		});
+	socket.on('jugandoAAhorcado', function(objeto){
+		if (!juegoMio){
+			console.log("Estoy jugando a ahorcado");
+			$('#formJuegoAhorcado').hide();
+			$('#formRespuestaJuego').show();
+			var fraseDev = objeto.fraseDevolver;
+			var numOp = objeto.numOp;
+			var anadir3 = `
+				${fraseDev}
+				<p>Quedan ${numOp} oportunidades</p>
+			`;
+			$('#pista').empty();
+			$('#pista').append(anadir3);
+			bajarScroll();	
+		}
+	});
 
-		var $botonImagenes = $("#imagenes");
-		$botonImagenes.off('click');
-		$botonImagenes.on('click', function(event){
-			event.preventDefault();
-			funImagen($atributo);
+	$('#formRespuestaJuego').on('submit', function(event) {
+		event.preventDefault();
+		var letra = $('#formRespuestaJuego input[name=respuestaAhorcado]').val();
+		$('#formRespuestaJuego').each(function(){
+			this.reset();
 		});
+		console.log('Probando una letra');
+		socket.emit('probarLetra',letra);
+	});
+
+	socket.on('comprobarLetra', function(objeto){
+		if (!juegoMio){
+			if (!objeto.nohayJuego){
+				var fraseDev = objeto.fraseDevolver;
+				var numOp = objeto.numOp;
+				var anadir3 = `
+					${fraseDev}
+					<p>Quedan ${numOp} oportunidades</p>
+				`;
+				$('#pista').empty();
+				$('#pista').append(anadir3);
+			}else{
+				$('#formRespuestaJuego').hide();
+				$('#formJuegoAhorcado').show();
+				jugando = false;
+				alert(objeto.mensaje2);
+				bajarScroll();
+			}
+		}else{
+			if (objeto.nohayJuego){
+				jugando = false;
+				juegoMio = false;
+				alert(objeto.mensaje2+" que pasa campeon");
+				$('#formJuegoAhorcado').show();
+				bajarScroll();
+			}
+		}
 	});
 });
